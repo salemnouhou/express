f4768a13fac4150190c3a9c831a5d3954ce84778
Sale Nouhou <salemnouhou2020@gmail.com>
2024-09-24 03:14:53 +0100

    [express2]

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 app.use(express.json());
 
 app.get('/authorize', (req, res) => {
-    const trelloAuthUrl = 'https://trello.com/1/authorize?expiration=1day&name=MyPersonalToken&scope=read&response_type=token&key=c44fc1d689c275f0c20f261b7827cfaa&return_url=https://localhost:3000/callback';
+    const trelloAuthUrl = 'https://trello.com/1/authorize?expiration=1day&name=MyPersonalToken&scope=read&response_type=token&key=24dae7ea7be0b916f47975a8e2316242&return_url=https://express-trello.onrender.com/callback';
     res.redirect(trelloAuthUrl);
 });
 
